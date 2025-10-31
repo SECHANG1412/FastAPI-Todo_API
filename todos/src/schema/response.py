e6512e047f7fc10cdafd0075c69433d337cae0db
SECHANG1412 <binaryblitz1412@gmail.com>
2025-10-31 20:17:32 +0900

    회원가입 API

--- todos/src/schema/response.py
+++ todos/src/schema/response.py
@@ -13,3 +13,11 @@ class ToDoSchema(BaseModel):
 
 class ToDoListSchema(BaseModel):
     todos: List[ToDoSchema]
+
+
+class UserSchema(BaseModel):
+    id: int
+    username: str
+
+    class Config:
+        orm_mode = True
